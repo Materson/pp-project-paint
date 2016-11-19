561b028da98cbecb454f9880d4ddc744b135233a
Mateusz Szymanowski <mathew.szyman@gmail.com>
2016-11-19 13:48:42 +0100

    Add struct coordinate_t and menu_t, gitignore *.depend

--- main.cpp
+++ main.cpp
@@ -3,56 +3,102 @@
 
 /* Uwaga: w docelowym programie nalezy zadeklarowac odpowiednie
    stale, na przyklad po to, aby wyeliminowac z programu
-   wartosci numeryczne umieszczajac w ich miejsce 
+   wartosci numeryczne umieszczajac w ich miejsce
    dobrze dobrane identyfikatory */
 
+#define START_C_X 40
+#define START_C_Y 12
+#define START_M_X 55
+#define START_M_Y 1
+
+#define UP 0x48
+#define DOWN 0x50
+#define LEFT 0x4b
+#define RIGHT 0x4d
+
+typedef struct
+{
+    int x;
+    int y;
+}coordinate_t;
+
+typedef struct
+{
+    char key_code[32] = "kod klawisza: 0x";
+    int arrows = 0;
+    coordinate_t co = {START_M_X, START_M_Y};
+}menu_t;
+
+
+
+void displayMenu(menu_t menu,char zn)
+{
+    textcolor(LIGHTGRAY);
+    gotoxy(menu.co.x, menu.co.y);
+    cputs("q = wyjscie");
+	gotoxy(menu.co.x, menu.co.y+2);
+	cputs("strzalki = poruszanie");
+	gotoxy(menu.co.x, menu.co.y+3);
+	cputs("spacja = zmiana koloru");
+	gotoxy(menu.co.x, menu.co.y+4);
+	cputs("enter = zmiana koloru tla");
+	if(menu.arrows) {
+		menu.key_code[16] = '0';
+		menu.key_code[17] = '0';
+		menu.key_code[18] = ' ';
+		menu.key_code[19] = '0';
+		menu.key_code[20] = 'x';
+		itoa(zn, menu.key_code + 21, 16);
+		menu.arrows=0;
+	} else {
+		itoa(zn, menu.key_code + 16, 16);
+		}
+	gotoxy(menu.co.x, menu.co.y+5);
+	cputs(menu.key_code);
+}
+
 int main() {
-	int zn = 0, x = 40, y = 12, attr = 7, back = 0, zero = 0;
-	char txt[32] = "kod klawisza: 0x";
+    menu_t menu;
+	int zn = 0, c_x = START_C_X, c_y = START_C_Y, attr = LIGHTGRAY, back = BLACK;
+
 	// je¿eli program jest kompilowany w czystym jêzyku C
 	// proszê odkomentowaæ poni¿sz¹ liniê
 	// Conio2_Init();
-	settitle("imie nazwisko nr_indeksu");
+	settitle("Mateusz Szymanowski 165319");
+
 	do {
 		textbackground(BLACK);
 		clrscr();
-		textcolor(7);
-		gotoxy(55, 1);
-		cputs("q = wyjscie");
-		gotoxy(55, 2);
-		cputs("strzalki = poruszanie");
-		gotoxy(55, 3);
-		cputs("spacja = zmiana koloru");
-		gotoxy(55, 4);
-		cputs("enter = zmiana koloru tla");
-		if(zero) {
-			txt[16] = '0';
-			txt[17] = '0';
-			txt[18] = ' ';
-			txt[19] = '0';
-			txt[20] = 'x';
-			itoa(zn, txt + 21, 16);
-		} else {
-			itoa(zn, txt + 16, 16);
-			}
-		gotoxy(55, 5);
-		cputs(txt);
-		gotoxy(x, y);
+
+		displayMenu(menu, zn);
+
+		gotoxy(c_x, c_y);
 		textcolor(attr);
 		textbackground(back);
 		putch('*');
-		zero = 0;
+
 		zn = getch();
-		if(zn == 0) {
-			zero = 1;
-			zn = getch();
-			if(zn == 0x48) y--;
-			else if(zn == 0x50) y++;
-			else if(zn == 0x4b) x--;
-			else if(zn == 0x4d) x++;
-		} else if(zn == ' ') attr = (attr + 1) % 16;
-		else if(zn == 0x0d) back = (back + 1) % 16;
+		switch(zn)
+		{
+        case 0:
+            menu.arrows = 1;
+            zn = getch();
+            if(zn == UP) c_y--;
+            else if(zn == DOWN) c_y++;
+            else if(zn == LEFT) c_x--;
+            else if(zn == RIGHT) c_x++;
+            break;
+        case ' ':
+             attr = (attr + 1) % 16;
+             break;
+        case 0x0d:
+            back = (back + 1) % 16;
+            break;
+		}
+
 	} while (zn != 'q');
 
+    textcolor(LIGHTGRAY);
+    textbackground(BLACK);
 	return 0;
 	}
